87f15bd140609edb7985be1d92bfda8afaa5e8d7
Anthony Rodrigues <Leonidous2@gmail.com>
2023-05-24 22:37:50 -0400

    fixed Login splash page, did some preliminary styling

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,5 +1,8 @@
 /** @type {import('tailwindcss').Config} */
-module.exports = {
+
+const withMT = require('@material-tailwind/react/utils/withMT');
+
+module.exports = withMT({
   content: ['./src/**/*.{js,ts,jsx,tsx}'],
   theme: {
     fontSize: {
@@ -42,4 +45,4 @@ module.exports = {
     },
   },
   plugins: [],
-};
+});
